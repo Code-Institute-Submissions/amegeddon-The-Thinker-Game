f1f942074e857da8f1e56555c4797aedda44db0f
amegeddon <ameescook@gmail.com>
2024-01-07 16:18:02 +0000

    Adds js to show correct answer on incorrect answer screen

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -158,7 +158,7 @@ function handleGuess() {
       if (attemptsLeft === 0) {
         backgroundImage();
         document.body.style.backgroundImage = "url('assets/images/freud.jpeg')";
-        noCigar.innerHTML = `<p>Close but no Cigar ..... However, as the Greatest of Freudian's once said; "From error to error, one discovers the entire truth and guessing games really are quite delightful”- Sigmund Freud</p>`;
+        noCigar.innerHTML = `<p>Close but no Cigar ... the correct Thinker was '${hiddenName}'. However, as the Greatest of Freudian's once said; "From error to error, one discovers the entire truth and guessing games really are quite delightful”- Sigmund Freud</p>`;
         hideOrDisplay(gameArea);
         hideOrDisplay(endGameSection);
         title.style.display = "none";
